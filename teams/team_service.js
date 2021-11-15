765fdc539fa31315cbc74855b33bca2246e42f09
krakhra <kiratrakhra@gmail.com>
2021-11-15 11:55:39 -0500

    added randomize team logic

--- teams/team_service.js
+++ teams/team_service.js
@@ -3,28 +3,64 @@ const dotenv = require("dotenv");
 
 dotenv.config();
 
+const random_teams = (members) => {
+  let obj = { team1: [], team2: [] };
+
+  members.map((e) => {
+    //if either team is full
+    if (obj["team1"].length === 5) {
+      obj["team2"].push(e);
+    } else if (obj["team2"].length === 5) {
+      obj["team1"].push(e);
+    //else randomize teams
+    } else {
+      // 50/50 
+      if (Math.random() === 0) {
+        obj["team1"].push(e);
+      } else {
+        obj["team2"].push(e);
+      }
+    }
+  });
+
+  return obj;
+};
+
 module.exports = {
   async create_teams(interaction, client) {
-
     //go through guild manager to get the guild
-    let guilds = await client.guilds.fetch(process.env.GUILD)
-    let channels = await guilds.channels.fetch()
+    let guilds = await client.guilds.fetch(process.env.GUILD);
+    let channels = await guilds.channels.fetch();
 
-    //find channel with ppl in voice 
-    channels.map((e)=>{
+    let target_channel = null;
+    //find channel with ppl in voice
+    channels.map((e) => {
       //need to change this to 10
-      if(e.members.size >= 1 && e.type === "GUILD_VOICE"){
-        console.log(e.name)
+      if (e.members.size >= 1 && e.type === "GUILD_VOICE") {
+        target_channel = e;
       }
-    })
+    });
 
-    
+    //get all members in target voice
+    //when done with this code use actual members
+    //let members = target_channel.members
+    let members = [
+      "Kira",
+      "Trevor",
+      "Andy",
+      "Dennis",
+      "Lai",
+      "William",
+      "Braden",
+      "Ryan",
+      "Daniel",
+      "Sean",
+    ];
 
-    //returns a Guildmember class
-    let member = guilds.me
+    let teams = random_teams(members);
 
-    //Get the channel id the member is currently in
-    //let channelId = member.voice.channelId
+    console.log(teams);
 
+    //create an object with the teams
   },
 };
